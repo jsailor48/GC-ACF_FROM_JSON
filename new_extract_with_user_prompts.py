ed76edb581b598e8ac7b162a8b1be163d7abf162
jsailor48 <johnsailor48@gmail.com>
2025-04-23 10:55:52 -0500

    Add files via upload

---
+++ new_extract_with_user_prompts.py
@@ -0,0 +1,26 @@
+import numpy as np
+from numpy import gradient
+import matplotlib.pyplot as plt
+import json
+File_path = input("Enter File Path, and or name if in the same Directory")
+with open(File_path , 'r') as file:
+  data=json.load(file)
+  gk_integral = data["Conductivity"] ["GK_Integral"]
+  time = data ["Conductivity"]["Time"]
+  #print (gk_integral)
+  #print (time)
+  stored_gk_integral_data = np.array(gk_integral)
+  stored_time_data = np.array(time)
+  # numerical approximation
+  Time_step = input("Enter time step between trajectory frames; eg 1000fs (enter numerical values only)")
+  dt = int(Time_step)
+  auto_derivative = gradient(stored_gk_integral_data, dt)
+Graph_Title = input("Enter Graph Title... EG ACF for 0.70 S/m run: ")
+
+plt.plot(stored_time_data,auto_derivative, label = Graph_Title )
+plt.xlabel('Time (fs)')
+plt.ylabel('Intensity')
+plt.legend()
+plt.show()
+
+#print(data)
